e2ec83c1ba6082358ec4030c27d8f81c202c2cdd
Tyler Brown <34142390+TylerFromVT@users.noreply.github.com>
2017-12-12 17:10:09 -0500

    Trying to test my timeline service by mocking http.

--- src/app/timeline-service/timeline.service.spec.ts
+++ src/app/timeline-service/timeline.service.spec.ts
@@ -1,18 +1,41 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { TimelineService } from './timeline.service';
-import {FormsModule} from '@angular/forms';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TimelineService} from './timeline.service';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {EventData} from '../event-data';
 
 describe('TimelineService', () => {
+  let timelineService: TimelineService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      providers: [TimelineService]
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        TimelineService
+      ]
     });
+    timelineService = TestBed.get(TimelineService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([TimelineService], (service: TimelineService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should perform http get', inject([TimelineService], (service: TimelineService) => {
+    service.get().subscribe((events: EventData[]) => {
+      console.log('gah');
+      expect(events[0].keywords[0]).toBe('keyword');
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/timeline');
+    req.flush([{keywords: ['keyword']}]);
+    httpMock.verify();
+  }));
 });
+
+
